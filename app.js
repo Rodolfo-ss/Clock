0998740d2e2654c1b6f93d515964eca578a461dc
unknown <rodolfcs@gmail.com>
2023-11-12 21:15:52 -0300

    clock

---
+++ app.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector('.clock')
+
+const updateClock = () => {
+    const present = new Date()
+    const hours = present.getHours()
+    const minutes = present.getMinutes()
+    const seconds = present.getSeconds()
+
+    const clockHtml = `
+        <span>${String(hours).length === 1 ? `0${hours}` : hours}</span> :
+        <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
+        <span>${String(seconds),length === 1 ? `0${seconds}` : seconds}</span>
+    `
+
+
+    clock.innerHTML = clockHtml
+}
+
+setInterval(updateClock, 1000)
